d3a4d2c72802f191100e87fc002b5cff05b763be
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-15 14:59:04 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
 mod cli;
+mod filesystem;
 
-use cli::Cli;
+use cli::{Cli, Commands};
 use clap::Parser;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Task {
+    date: String,
+    category: String,
+    is_done: bool,
+    task: String,
+}
 
 fn main() {
     let cli = Cli::parse();
 
+    match &cli.commands {
+        Some(Commands::Add { task, category }) => {
+            let json = r#"
+                {
+                    "date": "2022",
+                    "is_done": true,
+                    "category": "dev",
+                    "task": "some dev thing."
+                }
+                "#;
+
+            let task: Task = serde_json::from_str(json).unwrap();
+
+            println!("date: {}", task.date);
+            println!("category: {}", task.category);
+            println!("task: {}", task.task);
+            println!("is done: {}", task.is_done);
+
+        }
+        Some(Commands::Delete { task_id }) => {}
+        Some(Commands::Edit { task_id }) => {}
+        Some(Commands::List { category }) => {}
+        Some(Commands::Done { task_id }) => {}
+        Some(Commands::Undone { task_id }) => {}
+        Some(Commands::Clear { category }) => {}
+
+        None => {}
+    }
 }
