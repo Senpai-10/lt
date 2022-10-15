21f0b1fa9d77dc5767f227551e7d96d5fcd7d2a5
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-15 14:58:44 +0300

    filesystem

---
+++ src/filesystem.rs
@@ -0,0 +1,22 @@
+// fs wrapper
+
+use std::fs;
+
+// functions
+
+// pub create_file(file) -> Error
+// pub read_file(file) -> String
+// pub write_file(file, contents) -> Error
+// pub exists
+
+pub fn create_file(file: String, contents: &str) {
+    return fs::write(file, contents).expect("Failed to create file");
+}
+
+pub fn read_file(file: String) -> String {
+    return fs::read_to_string(file).expect("Unable to read file");
+}
+
+pub fn file_exists(file: String) -> bool {
+    return fs::metadata(file).is_ok();
+}
