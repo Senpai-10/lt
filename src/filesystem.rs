867850fc756a5a7607390ee718a59aa518c116af
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-17 18:52:34 +0300

    use &str

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -9,14 +9,14 @@ use std::fs;
 // pub write_file(file, contents) -> Error
 // pub exists
 
-pub fn create_file(file: String, contents: &str) {
+pub fn create_file(file: &str, contents: &str) {
     return fs::write(file, contents).expect("Failed to create file");
 }
 
-pub fn read_file(file: String) -> String {
+pub fn read_file(file: &str) -> String {
     return fs::read_to_string(file).expect("Unable to read file");
 }
 
-pub fn file_exists(file: String) -> bool {
+pub fn file_exists(file: &str) -> bool {
     return fs::metadata(file).is_ok();
 }
