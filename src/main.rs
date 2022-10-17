141e2afccd6c5a92b02b81bbc79b146dcfc8cd36
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-17 22:20:29 +0300

    list and add

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 // use this https://docs.rs/sqlite/0.27.0/sqlite/
+// https://github.com/rusqlite/rusqlite
 
 mod cli;
 mod filesystem;
 use clap::Parser;
 use cli::{Cli, Commands};
-// use sqlx::{sqlite::SqliteConnection, sqlite::SqliteRow, Connection, Sqlite};
+use rusqlite::{Connection, Result};
+use nanoid::nanoid;
 use dotenv::dotenv;
 
+#[derive(Debug)]
 struct Task {
     pub id: String,
     pub category: String,
@@ -14,53 +17,45 @@ struct Task {
     pub is_done: bool
 }
 
-#[async_std::main]
-async fn main() -> Result<(), sqlx::Error> {
+fn main() -> Result<()> {
     dotenv().ok();
     let cli = Cli::parse();
 
-    // let db_path: String = match std::env::var("DATABASE_URL") {
-    //     Ok(v) => {
-    //         v
-    //     },
-    //     Err(_) => {
-    //         "test.db".into()
-    //     }
-    // };
+    let db_file = match cli.file {
+        Some(file) => file,
+        None => String::from("todo.db")
+    };
 
-    // if filesystem::file_exists(&db_path) {
-    //     filesystem::create_file(&db_path, "")
-    // }
+    let conn = Connection::open(db_file)?;
 
-    // let mut conn = SqliteConnection::connect(&db_path).await?;
-
-    // sqlx::query(
-    //     r#"
-    //     CREATE TABLE IF NOT EXISTS tasks (
-    //         id          VARCHAR     NOT NULL PRIMARY KEY,
-    //         category    VARCHAR     NOT NULL,
-    //         text        TEXT        NOT NULL,
-    //         is_done     BOOLEAN     NOT NULL DEFAULT 'false'
-    //     )
-    // "#).execute(&mut conn).await?;
-
-    // sqlx::query(
-    //     r#"
-    //     INSERT INTO tasks (id, text) VALUES (1, 'some testing text')
-    // "#).execute(&mut conn).await?;
-
-    // sqlx::query(
-    //     r#"
-    //     INSERT INTO tasks (id, text) VALUES (2, 'some testing text number 2')
-    // "#).execute(&mut conn).await?;
-
-    // sqlx::query(
-    //     r#"
-    //     INSERT INTO tasks (id, text) VALUES (3, 'some testing text number 3')
-    // "#).execute(&mut conn).await?;
+    conn.execute(
+        r#"
+            CREATE TABLE IF NOT EXISTS tasks (
+                 id          VARCHAR     NOT NULL PRIMARY KEY,
+                 category    VARCHAR     NOT NULL,
+                 text        TEXT        NOT NULL,
+                 is_done     BOOLEAN     NOT NULL DEFAULT 'false'
+            )
+        "#,
+        (), // empty list of parameters.
+    )?;
 
     match &cli.commands {
-        Some(Commands::Add { task, category }) => {}
+        Some(Commands::Add { task, category }) => {
+            let id = nanoid!(8);
+
+            let new_task = Task {
+                id: id,
+                category: category.into(),
+                text: task.into(),
+                is_done: false
+            };
+
+            conn.execute(
+                "INSERT INTO tasks (id, category, text, is_done) VALUES (?1, ?2, ?3, ?4)",
+                (&new_task.id, &new_task.category, &new_task.text, &new_task.is_done),
+            )?;
+        }
 
         Some(Commands::Delete { task_id }) => {
             println!("{}", task_id)
@@ -71,18 +66,19 @@ async fn main() -> Result<(), sqlx::Error> {
         }
 
         Some(Commands::List { category }) => {
-            // let res: Vec<Task> = sqlx::query_as!(Task,
-            //     r#"
-            //     SELECT * FROM tasks
-            //     "#
-            // )
-            // .fetch_all(&mut conn)
-            // .await.unwrap();
+            let mut stmt = conn.prepare("SELECT * FROM tasks")?;
+            let tasks_iter = stmt.query_map([], |row| {
+                Ok(Task {
+                    id: row.get(0)?,
+                    category: row.get(1)?,
+                    text: row.get(2)?,
+                    is_done: row.get(3)?
+                })
+            })?;
 
-            // for task in res {
-            //     println!("id: {}", task.id);
-            //     println!("text: {}", task.text);
-            // }
+            for task in tasks_iter {
+                println!("Found task {:?}", task.unwrap());
+            }
         }
 
         Some(Commands::Done { task_id }) => {
