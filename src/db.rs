384dac95500f67072a37446ce2e85f6a6cbfbeff
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-19 17:11:44 +0300

    get_task

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,19 @@
-/// simple functions to access db
+use rusqlite::Connection;
+use crate::Task;
 
-pub struct Db {
-
+pub fn get_task(conn: &Connection, task_id: &String) -> Task {
+    conn.query_row(
+        "SELECT * FROM tasks WHERE id = ?",
+        [task_id],
+        |row| {
+            Ok(
+                Task {
+                    id: row.get(0)?,
+                    category: row.get(1)?,
+                    text: row.get(2)?,
+                    is_done: row.get(3)?,
+                }
+            )
+        },
+    ).unwrap()
 }
-
