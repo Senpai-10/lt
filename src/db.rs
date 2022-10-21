a335f9f0b36908e3a6a45d684608a7c0e27201ea
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-21 20:53:44 +0300

    add priority colmn

--- src/db.rs
+++ src/db.rs
@@ -7,10 +7,11 @@ pub fn setup(conn: &Connection) -> Result<usize, rusqlite::Error> {
     conn.execute(
         r#"
             CREATE TABLE IF NOT EXISTS tasks (
-                 id          VARCHAR     NOT NULL PRIMARY KEY,
-                 category    VARCHAR     NOT NULL,
-                 text        TEXT        NOT NULL,
-                 is_done     BOOLEAN     NOT NULL DEFAULT 'false'
+                id          VARCHAR     NOT NULL PRIMARY KEY,
+                category    VARCHAR     NOT NULL,
+                text        TEXT        NOT NULL,
+                is_done     BOOLEAN     NOT NULL DEFAULT 'false',
+                priority    INT         NOT NULL DEFAULT 1
             )
         "#,
         (),
