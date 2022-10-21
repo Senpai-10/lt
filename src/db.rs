ec0da09d55135c94fd45884c80473c743416f1be
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-21 22:58:58 +0300

    todo

--- src/db.rs
+++ src/db.rs
@@ -4,14 +4,16 @@ pub mod tasks;
 
 /// CREATE TABLE/S
 pub fn setup(conn: &Connection) -> Result<usize, rusqlite::Error> {
+    // find a way to ALTER table and add missing columns
     conn.execute(
         r#"
             CREATE TABLE IF NOT EXISTS tasks (
-                id          VARCHAR     NOT NULL PRIMARY KEY,
-                category    VARCHAR     NOT NULL,
-                text        TEXT        NOT NULL,
-                is_done     BOOLEAN     NOT NULL DEFAULT 'false',
-                priority    INT         NOT NULL DEFAULT 1
+                id          VARCHAR     NOT NULL PRIMARY KEY        ,
+                category    VARCHAR     NOT NULL                    ,
+                text        TEXT        NOT NULL                    ,
+                is_done     BOOLEAN     NOT NULL DEFAULT('false')   ,
+                priority    INT         NOT NULL DEFAULT(1)         ,
+                done_date   INT         NULL     DEFAULT(NULL)
             )
         "#,
         (),
