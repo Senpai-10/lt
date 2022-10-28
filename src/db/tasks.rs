4ba5cd715add80d5945df8085d9323bdfcdbc4fa
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-28 17:09:20 +0300

    remove is_done usage

--- src/db/tasks.rs
+++ src/db/tasks.rs
@@ -4,17 +4,55 @@ use crate::{
 };
 use colored::Colorize;
 use inquire::MultiSelect;
-use rusqlite::{params, Connection};
+use rusqlite::{params, Connection, types::FromSql};
 use std::cmp::Reverse;
 use std::fmt;
 use clap::ValueEnum;
+
+#[derive(Copy, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum Status {
+    Done,
+    Active,
+    Pending
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Status::Done => write!(f, "done"),
+            Status::Active => write!(f, "active"),
+            Status::Pending => write!(f, "pending"),
+        }
+    }
+}
+
+impl FromSql for Status {
+    fn column_result(value: rusqlite::types::ValueRef) -> rusqlite::types::FromSqlResult<Self> {
+        Ok(match value {
+            rusqlite::types::ValueRef::Text(v) => {
+                let str_status = std::str::from_utf8(v).unwrap();
+
+                if str_status == "done" {
+                    Status::Done
+                }
+                else if str_status == "active" {
+                    Status::Active
+                }
+                else {
+                    Status::Pending
+                }
+            },
+                _ => Status::Pending
+        })
+    }
+}
 
 #[derive(Debug)]
 pub struct Task {
     pub id: String,
     pub category: String,
     pub text: String,
-    pub is_done: bool,
+    pub status: Status,
     pub priority: i32,
     pub done_date: Option<u64>,
 }
@@ -34,12 +72,6 @@ impl fmt::Display for Filter {
             Filter::Pending => write!(f, "pending"),
         }
     }
-}
-
-pub enum Status {
-    Done,
-    Active,
-    Pending
 }
 
 pub struct TasksManager {
@@ -81,23 +113,33 @@ impl TasksManager {
         );
 
         for task in tasks {
-            let styled_is_done: String = match task.is_done {
-                true => {
-                    format!("{}", "".bright_green())
-                }
-                false => {
-                    format!("{}", "".bright_magenta())
-                }
+            let styled_is_done: String = match task.status {
+                Status::Done => {
+                    format!("{}", task.status.to_string().bright_green())
+                },
+                Status::Pending => {
+                    format!("{}", task.status.to_string().bright_magenta())
+                },
+                Status::Active => {
+                    format!("{}", task.status.to_string().bright_blue())
+                },
             };
 
-            let styled_text: String = match task.is_done {
-                true => task.text.strikethrough().to_string(),
-                false => task.text.to_string(),
+            let styled_text: String = match task.status {
+                Status::Done => {
+                    task.text.strikethrough().to_string()
+                },
+                Status::Pending => {
+                    task.text.to_string()
+                },
+                Status::Active => {
+                    task.text.to_string()
+                },
             };
 
             let done_date: String = match task.done_date {
                 Some(unix_timestamp) => {
-                    if !task.is_done {
+                    if task.status != Status::Done {
                         String::new()
                     } else {
                         let date = convert_unix_timestamp(unix_timestamp, date_format);
@@ -118,7 +160,7 @@ impl TasksManager {
 
             println!(
                 "  {}",
-                if task.is_done {
+                if task.status == Status::Done {
                     msg.bright_black().to_string()
                 } else {
                     match task.priority {
@@ -138,27 +180,35 @@ impl TasksManager {
         let mut options: Vec<String> = Vec::new();
 
         for task in tasks {
-            let styled_is_done: String = match task.is_done {
-                true => format!("{}", "DONE").bright_green().to_string(),
-                false => format!("{}", "PENDING").bright_magenta().to_string(),
+            let styled_is_done: String = match task.status {
+                Status::Done => format!("{}", "DONE").bright_green().to_string(),
+                Status::Pending => format!("{}", "PENDING").bright_magenta().to_string(),
+                Status::Active => format!("{}", "ACTIVE").bright_blue().to_string(),
             };
 
             let done_date: String = match task.done_date {
                 Some(unix_timestamp) => {
-                    if !task.is_done {
-                        String::new()
-                    } else {
+                    if task.status == Status::Done {
                         let date = convert_unix_timestamp(unix_timestamp, &self.config.date_format);
 
                         format!("{} ", date)
+                    } else {
+                        String::new()
                     }
                 }
                 None => String::new(),
             };
 
-            let styled_text: String = match task.is_done {
-                true => task.text.bright_black().strikethrough().to_string(),
-                false => task.text.to_string(),
+            let styled_text: String = match task.status {
+                Status::Done => {
+                    task.text.strikethrough().to_string()
+                },
+                Status::Pending => {
+                    task.text.to_string()
+                },
+                Status::Active => {
+                    task.text.to_string()
+                },
             };
 
             let msg = format!(
@@ -209,7 +259,7 @@ impl TasksManager {
                     id: row.get(0)?,
                     category: row.get(1)?,
                     text: row.get(2)?,
-                    is_done: row.get(3)?,
+                    status: row.get(3)?,
                     priority: row.get(4)?,
                     done_date: row.get(5).unwrap_or(None),
                 })
@@ -234,7 +284,7 @@ impl TasksManager {
                     id: row.get(0)?,
                     category: row.get(1)?,
                     text: row.get(2)?,
-                    is_done: row.get(3)?,
+                    status: row.get(3)?,
                     priority: row.get(4)?,
                     done_date: row.get(5).unwrap_or(None),
                 })
@@ -250,14 +300,14 @@ impl TasksManager {
         )
     }
 
-    pub fn update_is_done(&self, id: &String, value: bool) {
+    pub fn update_status(&self, id: &String, status: Status) {
         match self.conn.execute(
-            "UPDATE tasks SET is_done = ?1 WHERE id = ?2",
-            params![value, id],
+            "UPDATE tasks SET status = ?1 WHERE id = ?2",
+            params![status.to_string(), id],
         ) {
             Ok(rows_updated) => {
                 if rows_updated != 0 {
-                    if value == true {
+                    if status == Status::Done {
                         // if done
                         self.update_done_date(id);
                         self.update_priority(id, 0);
@@ -268,7 +318,7 @@ impl TasksManager {
 
                     println!(
                         "{}",
-                        format!("task {} is {}", id, if value { "done" } else { "undone" })
+                        format!("task {} is {}", id, if status == Status::Done { "done" } else { "undone" })
                             .bright_green()
                             .bold()
                     )
@@ -318,12 +368,12 @@ impl TasksManager {
 
     pub fn add_task(&self, new_task: Task) -> Result<usize, rusqlite::Error> {
         self.conn.execute(
-            "INSERT INTO tasks (id, category, text, is_done, priority) VALUES (?1, ?2, ?3, ?4, ?5)",
+            "INSERT INTO tasks (id, category, text, status, priority) VALUES (?1, ?2, ?3, ?4, ?5)",
             (
                 &new_task.id,
                 &new_task.category,
                 &new_task.text,
-                &new_task.is_done,
+                &new_task.status.to_string(),
                 &new_task.priority,
             ),
         )
