ff55b0f080db32bd6c758b6d5fdce9af234fe052
abdalrzag eisa <bmjfdrh@gmail.com>
2023-08-14 07:45:02 +0300

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,96 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { Category, Task } from "./types"
 import "./App.css";
 
+
+
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [data, setData] = useState<Task[]>();
+    const [categories, setCategories] = useState<Category[]>();
+    const [category, setCategory] = useState<string | null>(null);
+    const [newTaskInput, setNewTaskInput] = useState("");
+    const [newCategoryInput, setNewCategoryInput] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    const getTasks = () => {
+        if (category != null) {
+            invoke("get_tasks", { category: category }).then((res: any) => {
+                setData(res)
+            })
+        } else {
+            invoke("get_tasks").then((res: any) => {
+                setData(res)
+            })
+        }
+    }
 
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    const getCategories = () => {
+        invoke("get_categories")
+            .then((data: any) => {
+                setCategories(data)
+            })
+    }
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    useEffect(() => {
+        getCategories()
+    }, [])
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    useEffect(() => {
+        getTasks()
+    }, [category])
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
+    if (data == undefined || categories == undefined) {
+        return <h1>Loading</h1>
+    }
 
-      <p>{greetMsg}</p>
-    </div>
-  );
+    const addCategory = () => {
+        invoke("add_category", { name: newCategoryInput });
+        setNewCategoryInput("")
+        getCategories();
+    }
+
+    const addTask = () => {
+        if (newTaskInput == "") return
+
+        invoke("add_task", { title: newTaskInput, category: category == null ? "main" : category })
+        setNewTaskInput("")
+        getCategories()
+        getTasks()
+    }
+
+    return (
+        <div className="container">
+            <div className="side-bar">
+                <button onClick={() => setCategory(null)}>All</button>
+                {
+                    categories.map((x) => (
+                        <button key={x.name} onClick={() => setCategory(x.name)}>{x.name}</button>
+                    ))
+                }
+                <div>
+                    <input className="new-category-input" onChange={(e) => setNewCategoryInput(e.currentTarget.value)} value={newCategoryInput} placeholder="category.." />
+                    <button onClick={addCategory} style={{ fontWeight: "bold" }}>+</button>
+                </div>
+            </div>
+            <div className="main-content">
+                {
+                    data.length == 0 && category == null ? <div><p>empty list</p></div> : data.map((task) => (
+                        <div key={task.id}>
+                            <span>{task.title}</span>
+                        </div>
+                    ))
+                }
+                {
+                    category != null ? (
+                        <>
+                            <input onChange={(e) => setNewTaskInput(e.currentTarget.value)} value={newTaskInput} placeholder="task.." />
+                            <button onClick={addTask}>add</button>
+                        </>
+                    ) : null
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
