210d7ef611483149612cfc758bbefd3de899eab2
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-03 16:47:16 +0300

    Fix: use `DATABASE_URL` when in debug mode

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -5,7 +5,11 @@ use std::env;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
+    #[cfg(debug_assertions)]
     let database_url = env::var("DATABASE_URL").unwrap_or(".todo-lt".into());
+
+    #[cfg(not(debug_assertions))]
+    let database_url = ".todo-lt";
 
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
