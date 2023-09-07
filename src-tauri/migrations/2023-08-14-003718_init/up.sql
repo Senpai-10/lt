aa183a2b405a1bb4ea2de43cc4480a9406159130
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-07 21:45:05 +0300

    format

--- src-tauri/migrations/2023-08-14-003718_init/up.sql
+++ src-tauri/migrations/2023-08-14-003718_init/up.sql
@@ -1,17 +1,17 @@
 -- Your SQL goes here
 
 CREATE TABLE categories (
-    name VARCHAR(255) PRIMARY KEY NOT NULL
+    name                    VARCHAR(255) PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE tasks (
-    id VARCHAR PRIMARY KEY NOT NULL,
-    category_name VARCHAR(255) NOT NULL REFERENCES categories(name) ON DELETE CASCADE,
-    title TEXT NOT NULL,
-    desc TEXT NULL,
-    status INT NOT NULL,
-    priority INT NOT NULL DEFAULT(0),
-    creation_date INT NOT NULL,
-    completion_date INT,
-    modification_date INT NOT NULL
+    id                      VARCHAR PRIMARY KEY NOT NULL,
+    category_name           VARCHAR(255) NOT NULL REFERENCES categories(name) ON DELETE CASCADE,
+    title                   TEXT NOT NULL,
+    desc                    TEXT NULL,
+    status                  INT NOT NULL,
+    priority                INT NOT NULL DEFAULT(0),
+    creation_date           INT NOT NULL,
+    completion_date         INT,
+    modification_date       INT NOT NULL
 );
